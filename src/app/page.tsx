f9fd43b181662fa64c08d8457b8874399ab6a205
Supriya Gorai <supriyagorai1995@gmail.com>
2024-10-19 01:59:38 +0530

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,20 @@ import Header from "@/components/header";
 import TabButtons from "@/components/tabButtons";
 import DonorList from "@/components/donorList";
 import DonorForm from "@/components/form";
+import Footer from "@/components/footer";
 
 export default function BloodDonorApp() {
 	return (
-		<div className="min-h-screen bg-red-50 dark:bg-gray-800">
+		<div className="min-h-screen flex flex-col bg-red-50 dark:bg-gray-800">
 			<Header />
-			<main className="container mx-auto px-4 py-8">
+			<main className="container mx-auto px-4 py-8  flex-grow">
 				<Tabs defaultValue="search" className="max-w-4xl mx-auto">
 					<TabButtons />
 					<DonorList />
 					<DonorForm />
 				</Tabs>
 			</main>
+			<Footer />
 			<Toaster />
 		</div>
 	);
---
+++ src/components/footer.tsx
@@ -0,0 +1,50 @@
+/** @format */
+
+import React from "react";
+import { Linkedin, Github, Twitter, Heart, HeartIcon } from "lucide-react";
+
+export default function Footer() {
+	return (
+		<footer className="bg-red-600 text-white py-4 mt-12">
+			<div className="container mx-auto px-4">
+				<div className="flex flex-col md:flex-row justify-between items-center">
+					<div className="text-center md:text-left mb-4 md:mb-0">
+						<p className="text-sm flex">
+							Made with <HeartIcon className="h-4 w-4 text-white mx-2" /> by
+						</p>
+						<h2 className="text-2xl font-bold mt-1">Supriya Gorai</h2>
+					</div>
+					<div className="flex space-x-4">
+						<a
+							href="https://www.linkedin.com/in/supriya-gorai"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-red-200 transition-colors"
+						>
+							<Linkedin className="w-6 h-6" />
+							<span className="sr-only">LinkedIn</span>
+						</a>
+						<a
+							href="https://github.com/SupriyaGo"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-red-200 transition-colors"
+						>
+							<Github className="w-6 h-6" />
+							<span className="sr-only">GitHub</span>
+						</a>
+						<a
+							href="https://twitter.com/supriya_gorai"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="hover:text-red-200 transition-colors"
+						>
+							<Twitter className="w-6 h-6" />
+							<span className="sr-only">Twitter</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+}
